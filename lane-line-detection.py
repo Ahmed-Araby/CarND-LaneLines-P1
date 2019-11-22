f5b670f8a439fefbfc74b2b51e464558f77be505
Ahmed Araby <ahmedaraby605@gmail.com>
2019-11-22 17:04:56 +0200

    Update lane-line-detection.py

--- lane-line-detection.py
+++ lane-line-detection.py
@@ -8,6 +8,7 @@ import cv2
 from Lines import *
 from Region_of_interest import *
 
+
 """
     Notes:
     1 - opencv store images in BGR.
@@ -29,14 +30,26 @@ from Region_of_interest import *
     8 - ( very important )line shape don't infer it's slope here as the image cordinate system is reflected 
         so we look at them in terms changes in x and y and look at the relation between them 
         which make +ve slope looking be in the image as -ve slope 
-        and vice versus     
+        and vice versus
+    9 - if we did averge lines with slopes and intercept this will save the step of extrapolate.
+    
+    10 -
+          
 """
 
 """
     TO-DO:
     1 - search for the best way to specify the hyper parameters parameters
     2 - search for how to make the hyper parameters dynamic.
-    3 -   
+    3 - in avergining the lines we can take into account the the length of the lines as weighted average 
+        also we can take lines with some specific length into account 
+        also we can look at standard diviation of slopes which I don't get it's idea here !!?
+    4 - try to average slopes and intercepts instead of avergining coordinates points and see the difference 
+    5 - I need to make the lines more table across frames 
+    6 - I need to make the line more thicker 
+    7 - I need to cut of from the vanching points of the two line or low down the intersection.
+    8 - investigate every pipeline step and see which part tham make the lines unstable 
+      
 """
 
 """
@@ -52,6 +65,18 @@ from Region_of_interest import *
     2 - filter the image with gaussian filter
     3 - get edges using cany edge detector 
     4 - get region of interest
+    5 - detect hough lines 
+    6 - split the lines detected into right lane and left lane , ignore horizontal and vertical 
+    7 - average the lines to get only 1 line for each lane 
+    8 - draw the lines on a copy of the original image 
+    [9] - if we will work on video it we just will read the vidoe frame by frame then we will go throw the same pipeline  
+"""
+
+"""
+    To - Learn :
+    1 - coordinate geometry
+    2 - parametric equation of line in 3d and 2d space 
+    3 - line exterpolation , interpolation  
 """
 
 # we will use moving average as it make transaction between different detected lines
@@ -74,7 +99,7 @@ def gauss_blur(image):
     :return:  blurred image with gaussian filter to remove the noise from it
     """
     blured_image = cv2.GaussianBlur(image , (7 , 7) , 0)
-    display(blured_image)
+    #display(blured_image)
     return blured_image
 
 
@@ -88,32 +113,38 @@ def canny_edges(image):
     T2 = 150     # was 220 strong edge , ***************** could be extreme in some bad roads *****************
     ap_size = 3 # ***********************  what is this !???   **************************
     edges_image = cv2.Canny(image , T1 , T2 , apertureSize=ap_size , L2gradient=True)
-    display(edges_image)
+    #display(edges_image)
 
     return edges_image
 def processes_video():
-    video_name ='solidWhiteRight.mp4'
-    cap = cv2.VideoCapture("solidWhiteRight.mp4")
+    """
+    - how to locate the waiting time in ms that is proper to the frame per second we egt from the video ?
+    :return:
+    """
+    video_name ='test_videos/solidYellowLeft.mp4'
+    cap = cv2.VideoCapture(video_name)
     while True:
+        print("here")
         ret, frame = cap.read()
-        #print(ret)
-        #if ret == False:
-        #    break
+        print(ret)
+        if ret == False:
+            break
 
         # processes the frame
-        #processed_image = main(frame , True)
-        cv2.imshow("prcessed image " , frame)
-        cv2.waitKey(1)
-    cap.release()
+        processed_image = processes_image(frame , True)
+        cv2.imshow("prcessed image " , processed_image)
+        cv2.waitKey(15)
+
+    cap.release() # what does this do !?
     cv2.destroyAllWindows()
 
     return
 
-def main(image , video):
+def processes_image(image , video):
     global Avg_left_lane , Avg_right_lane
     original_image = image
     if video==False:
-        img_name = "solidWhiteCurve.jpg"
+        img_name = "test_images/solidWhiteCurve.jpg"
         # load the image
         original_image = cv2.imread(img_name , -1)
 
@@ -141,15 +172,17 @@ def main(image , video):
     Avg_left_lane = average_lines(left_lines , Avg_left_lane[0] , Avg_left_lane[1], Avg_left_lane[2] , Avg_left_lane[3])
     # right lane
     Avg_right_lane = average_lines(right_lines , Avg_right_lane[0] , Avg_right_lane[1], Avg_right_lane[2] , Avg_right_lane[3])
-    print(Avg_left_lane)
-    print(Avg_right_lane)
 
+    # extrapolate lines
+    ex_left_lane = exterpolate_line(Avg_left_lane , original_image)
+    ex_right_lane = exterpolate_line(Avg_right_lane , original_image)
     # draw lines
-    lines_image = draw_lines([Avg_left_lane] , original_image)
-    lines_image = draw_lines([Avg_right_lane] , lines_image)
-    display(original_image)
-    return
+    lines_image = draw_lines([ex_left_lane] , original_image)
+    lines_image = draw_lines([ex_right_lane] , lines_image)
 
-# C++ lover
-#main(None , False)
+    #display(lines_image)
+    return lines_image
+
+
+#processes_image(None , False)
 processes_video()
